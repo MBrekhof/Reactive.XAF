d1729bcd5ed5f3a6f42880a0348e5082ee90b100
apobekiaris <apostolis.bekiaris@gmail.com>
2026-02-02 14:10:43 +0200

    XpandModelEditor VS2026 missing dependecies #1106

--- tools/Xpand.XAF.ModelEditor/IDE/XVSIX64/ModelEditorCommand.cs
+++ tools/Xpand.XAF.ModelEditor/IDE/XVSIX64/ModelEditorCommand.cs
@@ -7,49 +7,45 @@ using ModelEditor;
 using Task = System.Threading.Tasks.Task;
 
 namespace XVSIX64 {
-	
-	internal sealed class ModelEditorCommand {
-		public const int CommandId = 0x0100;
-		public static readonly Guid CommandSet = new Guid("32dec059-e0ad-40a1-a5b1-8bebd1171a00");
-		private readonly AsyncPackage _package;
+    internal sealed class ModelEditorCommand {
+        public const int CommandId = 0x0100;
+        public static readonly Guid CommandSet = new Guid("32dec059-e0ad-40a1-a5b1-8bebd1171a00");
+        private readonly AsyncPackage _package;
 
-		private ModelEditorCommand(AsyncPackage package, OleMenuCommandService commandService) {
-			this._package = package ?? throw new ArgumentNullException(nameof(package));
-			commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+        private ModelEditorCommand(AsyncPackage package, OleMenuCommandService commandService) {
+            this._package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
 
-			var menuCommandID = new CommandID(CommandSet, CommandId);
-			var menuItem = new MenuCommand(this.ExecuteAsync, menuCommandID);
-			commandService.AddCommand(menuItem);
-		}
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.ExecuteAsync, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
 
-		public static async Task InitializeAsync(AsyncPackage package) {
-			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+        public static async Task InitializeAsync(AsyncPackage package) {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
 
-			OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
-			Instance = new ModelEditorCommand(package, commandService);
-		}
+            OleMenuCommandService commandService =
+                await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new ModelEditorCommand(package, commandService);
+        }
 
-		
-		public static ModelEditorCommand Instance {
-			get;
-			private set;
-		}
 
-		public IAsyncServiceProvider ServiceProvider => _package;
+        public static ModelEditorCommand Instance { get; private set; }
 
-		public static async Task InitializeAsync(XVSIX64Package package) {
-			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+        public IAsyncServiceProvider ServiceProvider => _package;
 
-			var commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
-			Instance = new ModelEditorCommand(package, commandService);
-			await XpandModelEditor.ExtractMEAsync();
-            
-		}
+        public static async Task InitializeAsync(XVSIX64Package package) {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
 
-		private async void ExecuteAsync(object sender, EventArgs e) {
-			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-			await XpandModelEditor.StartMEAsync();
-			XpandModelEditor.WriteSettings(((DTE2) Package.GetGlobalService(typeof(DTE))).Solution.FileName);
-		}
-	}
+            var commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new ModelEditorCommand(package, commandService);
+            await XpandModelEditor.ExtractMEAsync();
+        }
+
+        private async void ExecuteAsync(object sender, EventArgs e) {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            await XpandModelEditor.StartMEAsync();
+            XpandModelEditor.WriteSettings(((DTE2)Package.GetGlobalService(typeof(DTE))).Solution.FileName);
+        }
+    }
 }
